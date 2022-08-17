dc83d7f12ca80b149c44ca9fef16457b5a7028b7
Supream2 <100725250+Supream2@users.noreply.github.com>
2022-08-16 21:38:29 -0400

    Update sort.js

--- bougu/sort.js
+++ bougu/sort.js
@@ -71,7 +71,7 @@ if (armorCK) {
 	};
 	//耐性ソート
 	s.style.display = "block",s.title = "Sort by Resistances";
-	selectSet(s,["Res","Fire","Water","Thunder","Ice","Dragon"]);
+	selectSet(s,["Res","Fir","Wat","Thu","Ice","Dra"]);
 	tH.cells[3].appendChild(s.cloneNode(true));
 	//スロ制限
 	s.title = "Sort by Slot";
@@ -184,7 +184,7 @@ if (armorCK) {
 			N = tr.length,
 			x = [];
 		for (var i = 0; i  <N; i++) {
-			x[i] = tr[i].style.display ? [0] : [+tr[i].cells[3].childNodes[s1*2].nodeValue.substring(5)];
+			x[i] = tr[i].style.display ? [0] : [+tr[i].cells[3].childNodes[s1*2].nodeValue.substring(4)];
 			x[i].row = tr[i];
 		}
 		x.sort(function(a, b){return b - a});
