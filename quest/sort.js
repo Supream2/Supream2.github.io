150fcc05fd91b674426ca1f9d2ca6e7f20417820
Supream2 <100725250+Supream2@users.noreply.github.com>
2022-09-13 22:09:36 -0400

    Update sort.js

--- quest/sort.js
+++ quest/sort.js
@@ -48,7 +48,7 @@ tH.cells[6].appendChild(s.cloneNode(true));
 	if (e === "Target") {
 		return function(){return true};
 	} else {
-		var reg = new RegExp(">[\w]+\s"+e"\s[0-9]+");
+		var reg = new RegExp(/>[\w]+\s"+e"\s[0-9]+/);
 		return function (cell) {return reg.test(cell./*@if (@_jscript_version < 9) innerText @else@*/ textContent /*@end@*/);};
 	}
 };
