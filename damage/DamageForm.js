0f9a056f440ebe505803475d596782e86d2c26e9
Supream2 <100725250+Supream2@users.noreply.github.com>
2022-11-17 16:16:27 -0500

    Add files via upload

--- damage/DamageForm.js
+++ damage/DamageForm.js
@@ -1510,11 +1510,11 @@ case 9: //ガンランス
 		if (eq[I_aCLASS] === CLASSTYPE.Tenrou) hougekiType = this.c_tenrouGuns.value + "Shelling" + this.c_tenrouGunsLv.value;
 		var hougeki = WP_Info.Bullet[ hougekiType ],rengekiP = 0;
 		switch (hougeki.N.substring(0,3)) {
-		case "Normal":
+		case "通常型":
 			rengekiP = 9;break;
-		case "Long  ":
+		case "放射型":
 			rengekiP = 7;break;
-		case "Spread":
+		case "拡散型":
 			rengekiP = 8;break;
 		}
 		//赤ゲージでは砲撃、HB使用不可、龍撃砲可
