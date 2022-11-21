76bb00e2e945fdd9b31c4827cfd8c82db62ed602
Supream2 <100725250+Supream2@users.noreply.github.com>
2022-11-21 11:24:12 -0500

    Add files via upload

--- damage/DamageForm.js
+++ damage/DamageForm.js
@@ -1171,15 +1171,15 @@ case 10: //弓
 
 		//ビン
 		var bin = eq[I_aBIN],b = (CK_FULL ? " Coating" : ""),df = document.createDocumentFragment(),o = document.createElement("option");
-		if (bin.indexOf("Pwr") !== -1) o.setAttribute("value", "KG"),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("強撃"+b));
+		if (bin.indexOf("Pwr") !== -1) o.setAttribute("value", "KG"),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("Power"+b));
 		o.setAttribute("value", "11" + (bin.indexOf("Psn+") !== -1 ? bin.charAt(bin.indexOf("Psn+") + 2) : 0)),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("Psn"+b));
 		if (bin.indexOf("Poison") === -1) df.lastChild.style.backgroundColor = "lightpink";
-		o.setAttribute("value", "12" + (bin.indexOf("Par+") !== -1 ? bin.charAt(bin.indexOf("Par+") + 2) : 0)),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("麻痺"+b));
+		o.setAttribute("value", "12" + (bin.indexOf("Par+") !== -1 ? bin.charAt(bin.indexOf("Par+") + 2) : 0)),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("Para"+b));
 		if (bin.indexOf("Par") === -1) df.lastChild.style.backgroundColor = "lightpink";
-		o.setAttribute("value", "13" + (bin.indexOf("Slp+") !== -1 ? bin.charAt(bin.indexOf("Slp+") + 2) : 0)),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("睡眠"+b));
+		o.setAttribute("value", "13" + (bin.indexOf("Slp+") !== -1 ? bin.charAt(bin.indexOf("Slp+") + 2) : 0)),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("Sleep"+b));
 		if (bin.indexOf("Slp") === -1) df.lastChild.style.backgroundColor = "lightpink";
-		if (bin.indexOf("Bmb") !== -1) o.setAttribute("value", "BA"),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("爆撃"+b));
-		if (bin.indexOf("Imp") !== -1) o.setAttribute("value", "DA"),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("打撃"+b));
+		if (bin.indexOf("Bmb") !== -1) o.setAttribute("value", "BA"),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("Bomb"+b));
+		if (bin.indexOf("Imp") !== -1) o.setAttribute("value", "DA"),df.appendChild(o.cloneNode(false)),df.lastChild.appendChild(document.createTextNode("Impact"+b));
 		this.c_bin.length = 1,this.c_bin.appendChild(df);
 
 		this.cngTame();
